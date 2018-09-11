207e6dfee4c650be5a60e4d55258b49dc71404bb
Abhishek Sinha <avishkarabhishek786@gmail.com>
2018-09-11 23:05:53 +0530

    rtcperconnection est

--- routes.js
+++ routes.js
@@ -9,4 +9,19 @@ router.get('/', (req, res)=>{
     })
 })
 
+router.get('/rtcpeerconnection', (req, res)=>{
+    res.render('rtcpeerconnection.ejs', {
+        data: {},
+        errors: {},
+        title: 'WebRTC APP'
+    })
+})
+router.get('/peerjs', (req, res)=>{
+    res.render('peerjs.ejs', {
+        data: {},
+        errors: {},
+        title: 'Peer JS'
+    })
+})
+
 module.exports = router
